a5dded7810eaaa3c454baa288f834962ea91f11d
Diogo Carvalho <dcar84@gmail.com>
2022-05-31 15:22:36 +0000

    Add function to store username chosen by the player

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 # https://www.asciiart.eu/vehicles/navy - ascii battleship
+#https://patorjk.com/software/taag/#p=display&v=0&f=Slant&t=Battleships - ascii title
+
 INTRO = """
 
              ____        __  __  __          __    _           
@@ -16,10 +18,32 @@ INTRO = """
                               !      | |||
                             _/|     _/|-++'
                         +  +--|    |--|--|_ |-
-                     { /|__|  |/\__|  |--- |||__/
-                    +---------------___[}-_===_.'____               /\
-                ____`-' ||___-{]_| _[}-  |     |_[___\==--          \/   _
- __..._____--==/___]_|__|_____________________________[___\==--___,-----' .7
+                     { /|__|  |/\__|  |--- |||__/  _
+ __..._____--==/___]_|__|_____________________________[___\==--___,-----' 7
 |                                                                   BB-61/
  \_______________________________________________________________________|
+
+
+Welcome to Battleships, the naval strategy game!
+
         """
+
+def new_game():
+    """
+    Runs at the beggining of every new game. Prints the title, ascii battleship art and 
+    gives the player a welcome message. Executes the new player function. 
+    """
+    print(INTRO)
+    new_player()
+
+
+def new_player():
+    """
+    Prompts the player to enter a username and returns it.
+    """
+    username = input("Please enter your username:\n")
+    
+    return username
+
+
+new_game()
